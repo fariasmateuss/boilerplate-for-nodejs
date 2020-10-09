a06c6cdce50b640bff2c7c78bf15b1af8f202377
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-10-09 00:52:24 -0400

    Project initial setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+const port = 3333;
+
+app.use(routes);
+
+app.listen(port, () => console.log(`server is listening on ${port}`));
