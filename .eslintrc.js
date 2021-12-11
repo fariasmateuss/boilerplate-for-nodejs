a4f7aec163dc5742091695b8cb5ae766f46bee39
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-12-11 00:54:43 -0400

    chore: add ESLint recommended config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
+  root: true,
+  plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended'
   ]
