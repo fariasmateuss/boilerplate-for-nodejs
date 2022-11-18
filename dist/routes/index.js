1f05c56de7474a630b2ea91221a85a542d45c868
Mateus V. Farias <me@mateusfarias.com.br>
2022-11-17 22:22:35 -0400

    chore(git): add dist folder to .gitignore

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var routes = (0, express_1.Router)();
-routes.get('/', function (request, response) {
-    return response.json({ message: 'Hello World!' });
-});
-exports["default"] = routes;
